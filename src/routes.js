19b2de25ba344c06c209282a462ab49bc643f7d4
PamLeles <pamela_leles_sousa@outlook.com>
2022-04-07 18:53:25 -0300

    passed the user profile object to profile.ejs

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,21 @@ const routes = express.Router();
 //concatenar
 const views = __dirname + "/views/"
 
+const profile = {
+    name: "Pâmela ",
+    avatar: "https://avatars.githubusercontent.com/u/98628912?v=4",
+    "monthly-budget": 3000 ,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
 //req,res
 //render - renderizar
 routes.get('/', (req, res) => res.render( views +"index"));
 routes.get('/job', (req, res) => res.render( views +"job"));
 routes.get('/jobedit', (req, res) => res.render( views +"job-edit"));
-routes.get('/profile', (req, res) => res.render( views +"profile"));
+routes.get('/profile', (req, res) => res.render( views +"profile", { profile: profile}));
 
 
 module.exports = routes; 
