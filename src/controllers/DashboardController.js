c180d00a50297ea905ed6630054185008077d4b0
PamLeles <pamela_leles_sousa@outlook.com>
2022-04-18 16:31:26 -0300

    adding const status

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -6,10 +6,23 @@ module.exports = {
     index(req, res) {
         const jobs = Job.get();
         const profile = Profile.get();
+
+        let statusCount = {
+            progress: 0,
+            done:0,
+            total:jobs.length
+
+        }
+
         const updatedJobs = jobs.map((job) => {
             //ajustes no job
             const remaining = jobUtils.remainingDays(job);
             const status = remaining <= 0 ? 'done' : 'progress'; //ternario
+            
+            //status = done
+            //statusCount[done] += 1
+            //somando a quantidade de status
+            statusCount[status] += 1; 
     
             return { //espalhamento
                 ...job,
@@ -18,7 +31,7 @@ module.exports = {
                 budget: jobUtils.calculateBudget(job, profile["value-hour"])
             }
         })
-        return res.render("index", { jobs: updatedJobs });
+        return res.render("index", { jobs: updatedJobs, profile: profile, statutsCount: statusCount });
     }
 
 }
